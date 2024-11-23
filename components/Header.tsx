1c121cee2a41bbd7c749cf60c58e1397b41aef04
unknown <josegabriel.jgtc42@gmail.com>
2024-11-23 17:55:01 -0400

    Mobile responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,28 +6,27 @@ import { PiTwitchLogo } from "react-icons/pi";
 
 const Header = () => {
     return (
-        <div className='flex w-full h-screen overflow-hidden'>
-            <section className=' flex items-center justify-center w-2/5'>
+        <div className='flex flex-col md:flex-row w-full h-auto md:h-screen overflow-hidden'>
+            <section className='flex items-center justify-center w-full md:w-2/5 p-4'>
                 <Image
                     src="/assets/portrait.png"
                     alt="DotDager"
                     width={400} height={0}
-                    className='  border-solid border-2 border-orange-500 rounded-lg shadow-2xl' />
+                    className='border-solid border-2 border-orange-500 rounded-lg shadow-2xl' />
             </section>
-            <section className=' flex flex-col items-center justify-center w-3/5'>
-                <h1 className="heading text-center">Mariano Dot Dager<br/></h1>
-                <p className="sub-heading !max-w-3xl text-center">Programador, streamer, filósofo,<br/> guitarrero, gatofílico y amante de los pepinos</p>
-                <div className='flex p-10 gap-10'>
+            <section className='flex flex-col items-center justify-center w-full md:w-3/5 p-4'>
+                <h1 className="heading text-center text-3xl font-bold">Mariano Dot Dager</h1>
+                <p className="sub-heading text-base text-center max-w-3xl">Programador, streamer, filósofo,<br /> guitarrero, gatofílico y amante de los pepinos</p>
+                <div className='flex p-10 gap-8 justify-center'>
                     <a href={"https://www.instagram.com/dager.32/"} target="_blank">
-                    <FaInstagram size='2.5rem' />
+                        <FaInstagram size='2.5rem' />
                     </a>
                     <a href="https://www.youtube.com/@DotDager" target='blank'>
-                    <PiYoutubeLogo size='2.5rem'  />
+                        <PiYoutubeLogo size='2.5rem' />
                     </a>
-                    <a href='https://www.twitch.tv/dagerxiv'  target='blank'>
-                    <PiTwitchLogo size='2.5rem' />
+                    <a href='https://www.twitch.tv/dagerxiv' target='blank'>
+                        <PiTwitchLogo size='2.5rem' />
                     </a>
-                    
                 </div>
             </section>
         </div>
